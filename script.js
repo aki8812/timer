70ea521aa10564423a58add76080c29b649fc79f
aki8812 <akihuang0815@gmail.com>
2026-03-18 21:57:21 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -26,6 +26,34 @@ const alertCloseBtn = document.getElementById('alert-close-btn');
 
 let currentMode = 'stopwatch';
 
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+function playBeep(type = 'normal') {
+    if (audioCtx.state === 'suspended') audioCtx.resume();
+    const oscillator = audioCtx.createOscillator();
+    const gainNode = audioCtx.createGain();
+
+    oscillator.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+
+    if (type === 'finish') {
+        oscillator.type = 'sine';
+        oscillator.frequency.setValueAtTime(500, audioCtx.currentTime);
+        oscillator.frequency.exponentialRampToValueAtTime(1000, audioCtx.currentTime + 0.5);
+        gainNode.gain.setValueAtTime(0.3, audioCtx.currentTime);
+        gainNode.gain.exponentialRampToValueAtTime(0.01, audioCtx.currentTime + 0.5);
+        oscillator.start();
+        oscillator.stop(audioCtx.currentTime + 0.5);
+    } else {
+        oscillator.type = 'triangle';
+        oscillator.frequency.setValueAtTime(800, audioCtx.currentTime);
+        gainNode.gain.setValueAtTime(0.1, audioCtx.currentTime);
+        gainNode.gain.exponentialRampToValueAtTime(0.01, audioCtx.currentTime + 0.1);
+        oscillator.start();
+        oscillator.stop(audioCtx.currentTime + 0.1);
+    }
+}
+
 btnStopwatch.addEventListener('click', () => switchMode('stopwatch'));
 btnTimer.addEventListener('click', () => switchMode('timer'));
 
